a75e425fcac4d325ed07ba75f8602f9e246a9d36
jurpie <sherry.shi@mail.utoronto.ca>
2014-08-06 00:09:15 -0400

    initial

--- SudokuSolver/src/SudokuMain.java
+++ SudokuSolver/src/SudokuMain.java
@@ -1,14 +1,12 @@
 /**
- * 
- */
-
-/**
+ * A class with a main function that runs the Sudoku solver
  * @author Sherry
  *
  */
 public class SudokuMain {
 
 	/**
+	 * Runs the Sudoku solver
 	 * @param args
 	 */
 	public static void main(String[] args) {
