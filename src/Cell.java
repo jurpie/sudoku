b1544af7744352a99fc322e22ba3548e2917f8f0
jurpie <sherry.shi@mail.utoronto.ca>
2014-08-10 11:23:04 -0400

    Add Cell.java

---
+++ src/Cell.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sherry
+ *
+ */
+public class Cell {
+	private int[] domain;
+	
+	public int[] getDomain(){
+		return domain;
+	}
+	
+	private int solution = 0;
+	public int getSolution(){
+		return solution;
+	}
+	
+	private boolean solved;
+	public boolean isSolved(){
+		return solved;
+	}
+}
