a453ed6450b8fa6edab2c9758dbc9b4faa9b7f1a
Marc J <jutras.marc@gmail.com>
2014-08-10 11:19:01 -0400

    Directory Changes

---
+++ src/SudokuMain.java
@@ -0,0 +1,17 @@
+/**
+ * A class with a main function that runs the Sudoku solver
+ * @author Sherry
+ *
+ */
+public class SudokuMain {
+
+	/**
+	 * Runs the Sudoku solver
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
